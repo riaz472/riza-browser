98b46a8289d5b49d17ef08674295877065512078
Akusha Kumaar <akushakumaar@gmail.com>
2026-05-21 01:01:57 +0000

    The preview is endlessly stuck on the 'Starting app...' loading screen.

--- next.config.ts
+++ next.config.ts
@@ -13,20 +13,14 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
